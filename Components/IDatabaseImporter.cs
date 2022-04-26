64c663304b7dc51f8a845ef78b53da245a0b653c
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2022-04-26 10:47:57 +0200

    Created the fish class that gets its data from the database

---
+++ Components/IDatabaseImporter.cs
@@ -0,0 +1,24 @@
+﻿namespace DatabaseProjekt
+{
+    public interface IDatabaseImporter
+    {
+        /// <summary>
+        /// Open connection to databse
+        /// </summary>
+        void Open();
+        /// <summary>
+        /// Close connection to database
+        /// </summary>
+        void Close();
+        /// <summary>
+        /// Returns texture from database
+        /// </summary>
+        void GetTexture();
+        /// <summary>
+        /// Returns the Id
+        /// </summary>
+        /// <param name="type"></param>
+        void GetId(string type);
+
+    }
+}
