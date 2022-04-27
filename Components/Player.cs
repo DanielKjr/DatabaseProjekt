b332c116df2bb2ec1612d61ef779b81ef719c466
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2022-04-27 09:32:23 +0200

    Player can write to highscore

--- Components/Player.cs
+++ Components/Player.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.Xna.Framework;
+using System.Data.SQLite;
 
 namespace DatabaseProjekt
 {
-    public class Player : Component
+
+    public class Player : Component, IDatabaseImporter
     {
         private float speed;
-        private bool canShoot = true;
         private int userID;
         private Animator animator;
+        private int score;
+
 
         public int UserID { get => userID; set => userID = value; }
 
@@ -41,6 +44,55 @@ namespace DatabaseProjekt
         public override void Update(GameTime gameTime)
         {
             InputHandler.Instance.Execute(this);
+        }
+
+        public void Open()
+        {
+            GameWorld.Instance.connection.Open();
+        }
+
+        public void Close()
+        {
+            GameWorld.Instance.connection.Close();
+        }
+
+        public void GetTexture()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetId(string type)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetAttributes()
+        {
+            Open();
+            //indsæt værdier før det køres
+            var cmd = new SQLiteCommand("SELECT attributes FROM table", GameWorld.Instance.connection);
+            var dataread = cmd.ExecuteReader();
+
+            while (dataread.Read())
+            {
+               
+            }
+
+            Close();
+        }
+
+        /// <summary>
+        /// Adds a highscore entry with UserId and their Score.
+        /// </summary>
+        /// <param name="score"></param>
+        public void SaveHighScore(int score)
+        {
+            Open();
+
+            var cmd = new SQLiteCommand($"INSERT INTO highscore (Id, Score) VALUES ({userID}, {score})", GameWorld.Instance.connection);
+            cmd.ExecuteNonQuery();
+
+            Close();
         }
     }
 }
